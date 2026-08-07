fa2a28b2a315db551e0be2e3b61c79dfc418059d
tonguyennhatthien <thien.tnn.cc25tth@ntu.edu.vn>
2026-08-07 21:19:18 +0700

    Hoàn thiện bài 1-Biến và toán tử

--- BaiTap1_BienPhepToan/src/BaiTap1.java
+++ BaiTap1_BienPhepToan/src/BaiTap1.java
@@ -3,13 +3,15 @@ public class BaiTap1 {
 
 	public static void main(String[] args) {
     // 1. Khai báo các biến
-		int a = 15;
-		int b = 4;
+		double a = 15;
+		double b = 4;
 		double c = 3.5;
 	// 2. Tính toán
-		int tong = a+b;
+		double tong = a+b;
 		double thuong = a/b;
-		
+	// 3. Xuất ra
+		System.out.println("Tổng là " + tong);
+		System.out.print("Thuong là: " + thuong + "\n");
 	}
 
 }
