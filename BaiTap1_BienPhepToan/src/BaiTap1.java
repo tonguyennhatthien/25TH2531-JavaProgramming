e8ad2dbe06440f9fecaf2c6398276b8d174977b7
tonguyennhatthien <thien.tnn.cc25tth@ntu.edu.vn>
2026-08-07 21:06:58 +0700

    Thêm các lệnh khai báo, và tính toán

---
+++ BaiTap1_BienPhepToan/src/BaiTap1.java
@@ -0,0 +1,15 @@
+
+public class BaiTap1 {
+
+	public static void main(String[] args) {
+    // 1. Khai báo các biến
+		int a = 15;
+		int b = 4;
+		double c = 3.5;
+	// 2. Tính toán
+		int tong = a+b;
+		double thuong = a/b;
+		
+	}
+
+}
