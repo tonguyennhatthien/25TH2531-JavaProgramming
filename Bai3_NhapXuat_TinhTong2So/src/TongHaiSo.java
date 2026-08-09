8a1918747d598748635b988dc22bce9605cf69f8
tonguyennhatthien <thien.tnn.cc25tth@ntu.edu.vn>
2026-08-09 17:40:40 +0700

    Thêm mã nhập, khai báo biến

--- Bai3_NhapXuat_TinhTong2So/src/TongHaiSo.java
+++ Bai3_NhapXuat_TinhTong2So/src/TongHaiSo.java
@@ -2,7 +2,19 @@
 public class TongHaiSo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Khai báo
+		double num1, num2, sum;
+		
+		// Khai báo đối tượng Scanner
+	    Scanner banPhim = new Scanner(System.in);
+	    
+	    // In ra thông tin, mời User nhập số thứ nhất
+	    
+	    System.out.print("Mời nhập số thứ nhất");
+	    num1= banPhim.nextDouble();
+	    
+	    System.out.print("Mời nhập số thứ hai");
+	    num2= banPhim.nextDouble();
 
 	}
 
