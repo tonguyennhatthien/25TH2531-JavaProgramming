b4a67f667a2e7f785ec57caa5fea4cbc5a918449
tonguyennhatthien <thien.tnn.cc25tth@ntu.edu.vn>
2026-08-12 10:57:34 +0700

    Hoàn thành

--- ViDuArrayListSoNguyen/src/ntu/thientnn/runMain.java
+++ ViDuArrayListSoNguyen/src/ntu/thientnn/runMain.java
@@ -26,16 +26,38 @@ public class runMain {
 	
 	//b. In ra danh sách vừa nhập
 	System.out.print("DANH SÁCH VỪA NHẬP LÀ \n");
-	for (int i=0; i<dsSoNguyen.size(); i++) {
-		// lấy giá trị phần tử ở vị trí i, cất vào biến tạm
-		Integer tam = dsSoNguyen.get(i);
-		System.out.print(tam + " ");
-	}
+//	for (int i=0; i<dsSoNguyen.size(); i++) {
+//		// lấy giá trị phần tử ở vị trí i, cất vào biến tạm
+//		Integer tam = dsSoNguyen.get(i);
+//		System.out.print(tam + " ");
+//	}
 	 for (Integer x: dsSoNguyen) {
 		 
 		 System.out.print(x + " ");
 	 } 
-
+      
+	 //c. Đếm số phần tử chẵn
+	 int soLuongPTchan=0;
+	 for (Integer x: dsSoNguyen) {
+		 if (x % 2 ==0 ) soLuongPTchan=soLuongPTchan+1;
+//		 if (x % 2 ==0 ) soLuongPTchan++;
+	 }
+	 System.out.print("Số phần tử chẵn là: "+ soLuongPTchan);
+	 
+//	 hoặc
+//		for (int i=0; i<dsSoNguyen.size(); i++) {
+//		Integer tam = dsSoNguyen.get(i);
+//		if (tam % 2 ==0 ) soLuongPTchan=soLuongPTchan+1;
+//	}
+	 
+	 
+	 //d. Tính tổng các phần tử: Thuật toán cộng dồn
+	 int TongAll=0;
+	 for (Integer x: dsSoNguyen ) {
+		TongAll = TongAll +x;
+	 }
+	 
+	 System.out.print("Tổng tất cả các phần tử chẵn là: "+ TongAll);
 	}
 
 }
