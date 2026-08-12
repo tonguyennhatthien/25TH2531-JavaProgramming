8e77219142e64d9a7fe628e810c9cc0932e3968d
tonguyennhatthien <thien.tnn.cc25tth@ntu.edu.vn>
2026-08-12 10:36:31 +0700

    Xong chức năng Xuất danh sách

--- ViDuArrayListSoNguyen/src/ntu/thientnn/runMain.java
+++ ViDuArrayListSoNguyen/src/ntu/thientnn/runMain.java
@@ -1,6 +1,7 @@
 package ntu.thientnn;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class runMain {
 
@@ -8,6 +9,32 @@ public class runMain {
 		// Khai báo biến danh sách, chứa các số nguyên
 		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
 		
+		// a. Nhập 10 phần tử từ bàn phím
+		Scanner banPhim = new Scanner(System.in);
+		
+	for (int i=0; i<10; i++) {
+		// Nhập 1 phần tử
+		    // In ra một hướng dẫn cho người dùng 
+//		System.out.print("Mời nhập phần tử thứ " + i + ": ");
+		System.out.print("a[" + i + "]=");
+		    // Cho User nhập
+		Integer tam = banPhim.nextInt();
+		    // Thêm vào cuối danh sách
+		dsSoNguyen.add(tam);
+	}
+	
+	
+	//b. In ra danh sách vừa nhập
+	System.out.print("DANH SÁCH VỪA NHẬP LÀ \n");
+	for (int i=0; i<dsSoNguyen.size(); i++) {
+		// lấy giá trị phần tử ở vị trí i, cất vào biến tạm
+		Integer tam = dsSoNguyen.get(i);
+		System.out.print(tam + " ");
+	}
+	 for (Integer x: dsSoNguyen) {
+		 
+		 System.out.print(x + " ");
+	 } 
 
 	}
 
