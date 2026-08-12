7a7a8e82b98b2f16a0fb2dd5197d8a612abe5015
tonguyennhatthien <thien.tnn.cc25tth@ntu.edu.vn>
2026-08-12 09:44:25 +0700

    Dược chức năng 1.Nhập danh sách

---
+++ ViDuArrayListSoNguyen/src/ntu/thientnn/runMain.java
@@ -0,0 +1,14 @@
+package ntu.thientnn;
+
+import java.util.ArrayList;
+
+public class runMain {
+
+	public static void main(String[] args) {
+		// Khai báo biến danh sách, chứa các số nguyên
+		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+		
+
+	}
+
+}
